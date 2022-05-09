e9e68bfe7755a2825f714bd07bc28beb5def5b5d
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-05-09 12:44:35 +0200

    Filter, reduce, find

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import shoppingList from './shoppingList.js'
 
-console.log(shoppingList)
+const food = shoppingList.filter(item => item.category === 'food');
 
+const foodPrice = food.reduce((acc, item)=> acc + item.price, 0)
+
+console.log(foodPrice);
+
+const mostExpensive = shoppingList.find(item => item.price > 500);
+console.log(mostExpensive)
 
